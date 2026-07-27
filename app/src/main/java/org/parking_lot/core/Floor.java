18d921b4caf8704a1cbce46136c04ea9e170313e
Agnija Berzina <agni.berzina@gmail.com>
2026-07-27 14:37:07 +0100

    Updated Floor class

--- app/src/main/java/org/parking_lot/core/Floor.java
+++ app/src/main/java/org/parking_lot/core/Floor.java
@@ -1,12 +1,13 @@
 package org.parking_lot.core;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Floor {
-    private final HashSet<Spot> reservedSpots = new HashSet<>();
-    private final HashSet<Spot> regularSpots = new HashSet<>();
-    private final HashSet<Spot> compactSpots = new HashSet<>();
-    private final HashSet<Spot> largeSpots = new HashSet<>();
+    private final PriorityBlockingQueue<Spot> reservedSpots = new PriorityBlockingQueue<>();
+    private final PriorityBlockingQueue<Spot> regularSpots = new PriorityBlockingQueue<>();
+    private final PriorityBlockingQueue<Spot> compactSpots = new PriorityBlockingQueue<>();
+    private final PriorityBlockingQueue<Spot> largeSpots = new PriorityBlockingQueue<>();
     private final int floorNum;
     private final HashSet<Spot> spotSet = new HashSet<>();
 
@@ -20,29 +21,16 @@ public class Floor {
         this.spotSet.addAll(compactSpots);
         this.largeSpots.addAll(largeSpots);
         this.spotSet.addAll(largeSpots);
-
     }
 
-//    The Floor adds/creates the spots? The ParkingLot creates floors
-//    public void addSpot(Spot spot) {
-//        switch (spot.readCategory()) {
-//            case RESERVED -> reservedSpots.add(spot);
-//            case REGULAR -> regularSpots.add(spot);
-//            case COMPACT -> compactSpots.add(spot);
-//            case LARGE -> largeSpots.add(spot);
-//        }
-//    }
-//    searchVacancy (if vacancy found, begin to check in vehicle)
-
-    public Spot checkIn(Vehicle vehicle, HashSet<Spot> spotHashSet) {
+    public Spot checkIn(Vehicle vehicle, PriorityBlockingQueue<Spot> spotPriorityBlockingQueue) {
 //        check for empty-return null, remove the spot from set, checkin vehicle in that spot, return the spot
-        if (spotHashSet.isEmpty()) {
+        if (spotPriorityBlockingQueue.isEmpty()) {
            return null;
         }
-        Spot spot = spotHashSet.iterator().next();
+        Spot spot = spotPriorityBlockingQueue.poll();
         try {
             spot.checkIn(vehicle);
-            spotHashSet.remove(spot);
         } catch(Exception e) {
             return null;
         }
@@ -50,25 +38,24 @@ public class Floor {
     }
 
     public Spot searchVacancy(Vehicle vehicle) {
-//        if vehicle is car, how do we search through regular spots and then search through large spots?
         List<SpotCategory> spotCategories = List.of(SpotCategory.values());
-        Spot vacantSpot = null;
+        Spot spot = null;
         for (SpotCategory spotCategory : spotCategories) {
            if (spotCategory.getAllowedVehicles().contains(vehicle.readVehicleClass())) {
 
-               vacantSpot = switch (spotCategory) {
+               spot = switch (spotCategory) {
                    case SpotCategory.RESERVED -> checkIn(vehicle, reservedSpots);
                    case SpotCategory.REGULAR -> checkIn(vehicle, regularSpots);
                    case SpotCategory.COMPACT -> checkIn(vehicle, compactSpots);
                    case SpotCategory.LARGE -> checkIn(vehicle, largeSpots);
                };
-               if (vacantSpot == null) {
+               if (spot == null) {
                    throw new IllegalStateException("No vacant spots available");
                };
                break;
             }
         }
-        return vacantSpot;
+        return spot;
     }
     public void checkOut(Vehicle vehicle, Spot spot) {
         try {
@@ -77,4 +64,8 @@ public class Floor {
             throw new RuntimeException(e);
         }
     }
+    public double getBill(Spot spot) {
+        return spot.getBill();
+    }
+    public int getFloorNum() { return this.floorNum; }
 }
